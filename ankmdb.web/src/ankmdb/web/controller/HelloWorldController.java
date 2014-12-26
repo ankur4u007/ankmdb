d451c9a0d5a678d0eae35ae1fb18dc66e6fbd2d3
ankur4u007 <ankur4u007@gmail.com>
2014-12-26 20:28:44 +0530

    w2-c6

--- ankmdb.web/src/ankmdb/web/controller/HelloWorldController.java
+++ ankmdb.web/src/ankmdb/web/controller/HelloWorldController.java
@@ -1,22 +1,19 @@
 package ankmdb.web.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.ModelMap;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloWorldController {
 
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(final ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
 
-	public class HelloController{
-	 
-	   @RequestMapping(method = RequestMethod.GET)
-	   public String printHello(ModelMap model) {
-	      model.addAttribute("message", "Hello Spring MVC Framework!");
-
-	      return "hello";
-	   }
-
+		return "hello";
 	}
+
 }
